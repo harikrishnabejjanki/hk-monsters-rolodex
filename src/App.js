93b14e992ebce8f75ca4f94300e861c68899f094
Harikrishna Bejjanki <harikrishna.bejjanki@SLS.ads.valuelabs.net>
2019-09-17 18:02:28 +0530

    added shop preview and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
 import './App.css';
 import HomePage from '../src/pages/homepage/homepage';
+import ShopPage from '../src/pages/shop/shop';
+import Header from './components/header/header';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+      {/* <HomePage /> */}
     </div>
   );
 }
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory';
+ 
+
 
 
 
 const homepage = () => (
+
     <div className="homepage">
+      
         <Directory />
     </div>
 );
